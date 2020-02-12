6f79fcd9b61d4b04c3611f387753b19d929227b8
ilhov.space__macbook <xmvmoon@gmail.com>
2020-02-12 16:31:18 +0200

    version 0.1.1

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -15,10 +15,10 @@ function Search(props){
         <div>
             <nav className="navbar navbar-light bg-light breadcrumb">
                 
-            <span className="navbar-brand">
+            <div className="navbar-brand">
                 <img src='https://t-rbt.telesens.ua/t-rbt/img/rbt-logo.png' alt={'rtb-logo'} style={{height:'40px'}}></img>
-                Your favorite melodies as a ring back tone!
-            </span>
+                <p>Your favorite melodies as a ringtone!</p>
+            </div>
             <form className="form-inline" onSubmit={submit}>
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
